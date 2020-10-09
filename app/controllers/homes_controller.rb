70d6386017e8de92132373f297cd2124f42719f9
Aka-5 <dassie.mathieu@gmail.com>
2020-10-09 20:30:48 +0200

    cleannup

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,2 +1,24 @@
 class HomesController < ApplicationController
-end
+
+  def show
+
+    end
+  def index
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def new
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  end
